1e09c4bd673892fcce260a4e72b3ed8dab9f12ca
Ankur Nath <anath@csce-44q58y3.engr.tamu.edu>
2024-05-28 11:40:00 -0500

    all models trained

--- train_LSDQN.sh
+++ train_LSDQN.sh
@@ -13,3 +13,20 @@ python train.py --distribution BA_40 --number_of_vertices 40 --model LSDQN --dev
 python train.py --distribution BA_60 --number_of_vertices 60 --model LSDQN --device 1
 python train.py --distribution BA_100 --number_of_vertices 100 --model LSDQN --device 1
 python train.py --distribution BA_200 --number_of_vertices 200 --model LSDQN --device 1
+
+
+python train.py --distribution HomleKim_200vertices_weighted  --model S2V
+python train.py --distribution HomleKim_200vertices_unweighted  --model S2V
+python train.py --distribution WattsStrogatz_200vertices_unweighted  --model S2V
+python train.py --distribution WattsStrogatz_200vertices_weighted  --model S2V
+
+python train.py --distribution HomleKim_200vertices_weighted  --model LSDQN 
+python train.py --distribution HomleKim_200vertices_unweighted  --model LSDQN 
+python train.py --distribution WattsStrogatz_200vertices_unweighted  --model LSDQN 
+python train.py --distribution WattsStrogatz_200vertices_weighted  --model LSDQN
+
+python train.py --distribution HomleKim_200vertices_weighted  --model LinearRegression
+python train.py --distribution HomleKim_200vertices_unweighted  --model LinearRegression
+python train.py --distribution WattsStrogatz_200vertices_unweighted  --model LinearRegression
+python train.py --distribution WattsStrogatz_200vertices_weighted  --model LinearRegression
+
